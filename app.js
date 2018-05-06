81a2e6025154cf9478186e741dea12e32ae5e881
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-05 22:05:48 -0700

    Adding all the new modules

---
+++ app.js
@@ -0,0 +1,22 @@
+
+//BUDGET CONTROLLER
+var budgetController = (function(){
+
+})();
+
+
+//UI CONTROLLER
+var UIController = (function(){
+
+    //some code later
+
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+    document.querySelector(".add__btn").addEventListener("click", function(){
+        
+    });
+
+})(budgetController, UIController);
