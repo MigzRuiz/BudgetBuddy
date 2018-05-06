4ae140bfca27efe006a5158dc9ae76c5e0924da9
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-05 22:26:08 -0700

    Adding the changes on the controller module

--- app.js
+++ app.js
@@ -10,13 +10,35 @@ var UIController = (function(){
 
     //some code later
 
+
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    document.querySelector(".add__btn").addEventListener("click", function(){
+    var ctrlAddItem = function() {
         
+        //1.) Get data (description and value)
+
+        //2.) Assign that data to internal data structure (budget controller)
+
+        //3.) Display those data (ui controller)
+
+        //4.) Calculate Budget
+
+        //5.) Display Budget
+    }
+
+    //When button is clicked 
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+
+    //When enter is pressed
+    document.addEventListener("keypress", function(event){
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        };
+
     });
 
 })(budgetController, UIController);
