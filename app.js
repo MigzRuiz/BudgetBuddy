4e7a754fefeb35d35d4f221a2dbd2018a64ccb01
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-05 22:55:48 -0700

    Adding ability to read data in the UI Controller

--- app.js
+++ app.js
@@ -8,17 +8,39 @@ var budgetController = (function(){
 //UI CONTROLLER
 var UIController = (function(){
 
-    //some code later
+    var DOMStrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    }
 
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, //either income or expense
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMStrings;
+        }
+    };
 
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
         
-        //1.) Get data (description and value)
+        //1.) Get input fields (type, description, value)
+        var input = UICtrl.getInput();
+        console.log(input);
 
         //2.) Assign that data to internal data structure (budget controller)
 
@@ -30,7 +52,7 @@ var controller = (function(budgetCtrl, UICtrl){
     }
 
     //When button is clicked 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     //When enter is pressed
     document.addEventListener("keypress", function(event){
